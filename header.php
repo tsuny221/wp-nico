a40e74556876d943906de07f4cd09f60c0bb2085
tsuny221 <kingoftsuny@gmail.com>
2020-10-23 13:07:20 +0900

    [Update] hamburger

--- header.php
+++ header.php
@@ -9,31 +9,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/><?php wp_head(); ?>
   </head>
   <body>
-    <header class="header">
-      <div class="header__logo"><a href=""><img class="header__logo__img" src="/portfolio/wp-content/themes/portfolio_template/images/logo.png" alt=""/></a>
-        <p class="header__logo__subtitle">ビスコッティ専門店</p>
-      </div>
-      <nav class="header__nav">
-        <ul class="header__nav__ul">
-          <li class="header__nav__li"><a href="#concept" title="コンセプト"><img class="header__nav__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav1.png"/></a></li>
-          <li class="header__nav__li"><a href="#secret" title="おいしさのひみつ"><img class="header__nav__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav2.png"/></a></li>
-          <li class="header__nav__li"><a href="#item-info" title="商品情報"><img class="header__nav__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav3.png"/></a></li>
-          <li class="header__nav__li"><a href="#shop-info" title="出店情報"><img class="header__nav__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav4.png"/></a></li>
-          <li class="header__nav__li"><a href="#online-shop" title="オンラインショップ"><img class="header__nav__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav5.png"/></a></li>
-        </ul>
-      </nav>
-      <div id="nav-drawer">
-        <input class="nav-unshown" id="nav-input" type="checkbox"/>
-        <label id="nav-open" for="nav-input"><img class="hamburger_img" src="images/hamburger.png"/></label>
-        <label class="nav-unshown" id="nav-close" for="nav-input"><img class="close_img" src="images/close.png"/></label>
-        <div id="nav-content">
-          <ul class="hamburger_nav_list">
-            <li><a href="#concept" title="コンセプト"><img src="images/biscottinav1.png"/></a></li>
-            <li><a href="#secret" title="おいしさのひみつ"><img src="images/biscottinav2.png"/></a></li>
-            <li><a href="#item-info" title="商品情報"><img src="images/biscottinav3.png"/></a></li>
-            <li><a href="#shop-info" title="出店情報"><img src="images/biscottinav4.png"/></a></li>
-            <li><a href="#online-shop" title="オンラインショップ"><img src="images/biscottinav5.png"/></a></li>
+    <header>
+      <div class="header">
+        <div class="header__logo"><a href=""><img class="header__logo__img" src="/portfolio/wp-content/themes/portfolio_template/images/logo.png" alt=""/></a>
+          <p class="header__logo__subtitle">ビスコッティ専門店</p>
+        </div>
+        <nav class="header__nav">
+          <ul class="header__nav__ul">
+            <li class="header__nav__li"><a href="#concept" title="コンセプト"><img class="header__nav__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav1.png"/></a></li>
+            <li class="header__nav__li"><a href="#secret" title="おいしさのひみつ"><img class="header__nav__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav2.png"/></a></li>
+            <li class="header__nav__li"><a href="#item-info" title="商品情報"><img class="header__nav__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav3.png"/></a></li>
+            <li class="header__nav__li"><a href="#shop-info" title="出店情報"><img class="header__nav__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav4.png"/></a></li>
+            <li class="header__nav__li"><a href="#online-shop" title="オンラインショップ"><img class="header__nav__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav5.png"/></a></li>
           </ul>
+        </nav>
+        <div class="mobile-menu" id="mobile-menu">
+          <input class="mobile-menu__input" id="input" type="checkbox"/>
+          <label for="input"><img class="mobile-menu__btn" src="/portfolio/wp-content/themes/portfolio_template/images/hamburger.png" alt=""/></label>
+        </div>
+        <div class="mobile-menu__content">
+          <nav class="mobile-menu__nav">
+            <ul class="mobile-menu__ul">
+              <li class="mobile-menu__li"><a href="#concept" title="コンセプト"><img class="mobile-menu__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav1.png"/></a></li>
+              <li class="mobile-menu__li"><a href="#secret" title="おいしさのひみつ"><img class="mobile-menu__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav2.png"/></a></li>
+              <li class="mobile-menu__li"><a href="#item-info" title="商品情報"><img class="mobile-menu__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav3.png"/></a></li>
+              <li class="mobile-menu__li"><a href="#shop-info" title="出店情報"><img class="mobile-menu__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav4.png"/></a></li>
+              <li class="mobile-menu__li"><a href="#online-shop" title="オンラインショップ"><img class="mobile-menu__img" src="/portfolio/wp-content/themes/portfolio_template/images/biscottinav5.png"/></a></li>
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
